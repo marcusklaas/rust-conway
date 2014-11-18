61657270741c00fdef1fab035ee8361278c5aaa6
Marcus Klaas <mail@marcusklaas.nl>
2014-11-18 01:24:53 +0100

    Functionally implement concurrency. Cleanup printing.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate ncurses;
 use std::comm::{Sender, Receiver};
 use std::cmp::max;
 use std::mem::swap;
-use ncurses::*;
 
 pub struct DuplexChannel<T: Send> {
     top_sender: Option<Sender<T>>,
@@ -28,8 +27,8 @@ impl<T: Send> DuplexChannel<T> {
         let mut result_vector = Vec::from_fn(number, |_| DuplexChannel::new());
         
         for i in range(0, max(1, number) - 1) {
-            let (bottom_sender, top_receiver): (Sender<T>, Receiver<T>) = channel();
-            let (top_sender, bottom_receiver): (Sender<T>, Receiver<T>) = channel();
+            let (bottom_sender, top_receiver) = channel::<T>();
+            let (top_sender, bottom_receiver) = channel::<T>();
             
             result_vector[i].bottom_sender = Some(bottom_sender);
             result_vector[i].bottom_receiver = Some(bottom_receiver);
@@ -70,9 +69,9 @@ impl<T: Send> DuplexChannel<T> {
 }
 
 pub struct GameState {
-    rows: uint,
-    columns: uint,
-    front: Vec<u8>,
+    pub rows: uint,
+    pub columns: uint,
+    pub front: Vec<u8>,
     back: Vec<u8>
 }
 
@@ -86,6 +85,36 @@ impl GameState {
             front: Vec::from_elem(element_count, 0u8),
             back: Vec::from_elem(element_count, 0u8)
         }
+    }
+    
+    pub fn from_parts(parts: &Vec<GameState>) -> Option<GameState> {
+        if parts.len() == 0 {
+            return None;
+        }
+        
+        let columns = parts[0].columns;
+        let mut front = Vec::from_elem(columns + 2, 0u8);
+        
+        for state in parts.iter() {        
+            for elem in state.front.slice_or_fail(&(columns + 2), &((state.rows + 1) * (columns + 2)))
+              .iter().map(|&x| x) {
+                front.push(elem);
+            }
+        }
+        
+        for _ in range(0, columns + 2) {
+            front.push(0u8);
+        }
+        
+        let elem_count = front.len();
+        let rows = elem_count/ (columns + 2) - 2;
+        
+        Some(GameState {
+            rows: rows,
+            columns: columns,
+            front: front,
+            back: Vec::from_elem(elem_count, 0u8)
+        })
     }
 
     fn next(&mut self) {
@@ -135,20 +164,16 @@ impl GameState {
         }
     }
     
-    pub fn print(&self) {
-        erase();
-    
+    pub fn print(&self, print_funk: |uint, uint| -> ()) {
         for row in range(0, self.rows) {
             for column in range(0, self.columns) {
                 let index = self.get_index(row, column);
             
                 if 1u8 == self.front[index] {
-                    mvaddch(row as i32, column as i32, '#' as u32);
+                    print_funk(row, column);
                 }
             }
         }
-        
-        refresh();
     }
     
     pub fn add_glider(&mut self, row: uint, column: uint) {
@@ -169,30 +194,50 @@ impl GameState {
         }
     }
     
-    // hier klopt nog niet zoveel van
-    pub fn split(&self, pieces: uint) -> Result<Vec<GameState>, &'static str> {
-        let total_rows = self.rows + 2;
-        
-        if pieces > total_rows {
+    pub fn split(&self, pieces: uint) -> Result<Vec<GameState>, &'static str> {        
+        if pieces > self.rows {
             return Err("Not enough rows to split into this many pieces!");
         }
         
-        let mut result = Vec::new();
-        
-        for i in range(0, pieces) {
-            let row_low = total_rows * i / pieces; // inclusive!
-            let row_high = total_rows * (i + 1) / pieces; // exclusive!
+        Ok(Vec::from_fn(pieces, |i| {
+            let row_low = self.rows * i / pieces; // inclusive!
+            let row_high = self.rows * (i + 1) / pieces; // exclusive!
+            let offset = (self.columns + 2) * (row_low + 1);
+            
             let mut part = GameState::new(row_high - row_low, self.columns);
             
-            part.front.clear();
+            for j in range(0, (row_high - row_low) * (self.columns + 2)) {
+                let index = self.columns + j;
             
-            for j in range(row_low * self.columns, row_high * self.columns) {
-                part.front.push(self.front[j]);
+                part.front[index] = self.front[offset + j];
             }
             
-            result.push(part);
+            part
+        }))
+    }
+    
+    pub fn read_top(&self) -> Vec<u8> {
+        self.read_line(1)
+    }
+    
+    pub fn read_bottom(&self) -> Vec<u8> {
+        self.read_line(self.rows)
+    }
+    
+    // line number starts at 1. returns self.columns + 2 elements
+    fn read_line(&self, line: uint) -> Vec<u8> {
+        self.front.slice_or_fail(&(line * (self.columns + 2)), &((line + 1) * (self.columns + 2))).iter().map(|&x| x).collect()
+    }
+    
+    pub fn set_top(&mut self, line: &Vec<u8>) {
+        for (i, value) in line.iter().enumerate() {
+            self.front[i] = *value;
         }
-        
-        Ok(result)
+    }
+    
+    pub fn set_bottom(&mut self, line: &Vec<u8>) {
+        for (i, value) in line.iter().enumerate() {
+            self.front[(1 + self.rows) * (self.columns + 2) + i] = *value;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,6 @@ fn main() {
     let thread_count: uint = from_str::<uint>(matches.opt_str("t").unwrap().as_slice()).unwrap();
     
     test_animation();
-    
-    test_concurrency(thread_count);
 }
 
 fn test_animation() {
@@ -40,61 +38,78 @@ fn test_animation() {
 
     initscr();
     curs_set(CURSOR_INVISIBLE);
-    cbreak(); // enable <Ctrl+C> to kill game
+    cbreak(); // enable <Ctrl+C> to kill program
     noecho(); // don't show input
     getmaxyx(stdscr, &mut height, &mut width);
-    timeout(2000);
 
-    let mut state = GameState::new(width as uint, height as uint);
+    let mut state = GameState::new(height as uint, width as uint);
     
     state.add_glider(1, 5);
     
     let mut timer = Timer::new().unwrap();
     let periodic = timer.periodic(Duration::milliseconds(40));
     
-    for _ in range(0, 100u) {
-        state.print();
+    for time in range(0, 180u) {
+        erase();
+        
+        //state.print();
+        
+        let new_state = progress_in_parallel(&state, time, 5);
+        
+        assert!(new_state.columns == width as uint);
+        assert!(new_state.rows == height as uint);
+        
+        new_state.print(|row, column| { 
+            mvaddch(row as i32, column as i32, '#' as u32);
+        });
+        
+        refresh();
         periodic.recv();
         
-        state.progress(1);        
+        //state.progress(1);        
     }
     
     endwin();
 }
 
-fn test_concurrency(thread_count: uint) {
-    let mut channels: Vec<DuplexChannel<uint>> = DuplexChannel::get_chain(thread_count);
+fn progress_in_parallel(state: &GameState, steps: uint, thread_count: uint) -> GameState {
+    let mut state_list = state.split(thread_count).unwrap();
+    let mut channels: Vec<DuplexChannel<Vec<u8>>> = DuplexChannel::get_chain(thread_count);
+    let (result_sender, result_receiver) = channel::<(uint, GameState)>();
     
     for i in range(0, thread_count).rev() {
         let channel = channels.pop().unwrap();
+        let mut state = state_list.pop().unwrap();
+        let my_result_sender = result_sender.clone();
     
-        spawn(proc() {
-            println!("Started task {}!", i);
-        
-            channel.send_top(i);
-            channel.send_bottom(i);
-        
-            match channel.receive_top() {
-                Some(x) => println!("Task {} received top message from task {}!", i, x),
-                None    => ()
-            };
+        spawn(proc() {        
+            for _ in range(0, steps) {
+                channel.send_top(state.read_top());
+                channel.send_bottom(state.read_bottom());
             
-            match channel.receive_bottom() {
-                Some(x) => println!("Task {} received bottom message from task {}!", i, x),
-                None    => ()
-            };
+                match channel.receive_top() {
+                    Some(x) => state.set_top(&x),
+                    None    => ()
+                };
+                
+                match channel.receive_bottom() {
+                    Some(x) => state.set_bottom(&x),
+                    None    => ()
+                };
+                
+                state.progress(1)
+            }
+            
+            my_result_sender.send((i, state));
         });
     }
-}
-
-static BENCH_SIZE: uint = 2;
-
-#[bench]
-fn naive_implementation(b: &mut Bencher) {
-    let mut state = GameState::new(300, 150);
     
-    state.add_glider(1, 5);
+    let mut result_vec = Vec::from_fn(thread_count, |_| result_receiver.recv());
     
-    b.iter(|| state.progress(BENCH_SIZE))
+    result_vec.sort_by(|&(i,_), &(j,_)| i.cmp(&j));
+    
+    let state_vec = result_vec.into_iter().map(|(_, state)| state).collect();
+    
+    GameState::from_parts(&state_vec).unwrap()
 }
 
